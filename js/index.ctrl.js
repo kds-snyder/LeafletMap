0e20093c98ef7305fba3bb7319e2893e3de659e5
Karen Snyder <karen.d.snyder@advantagesolutions.net>
2019-09-05 10:27:12 -0700

    Changed map to display US with San Diego marker and popup

--- js/index.ctrl.js
+++ js/index.ctrl.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var mapBoxAccessToken = 'pk.eyJ1Ijoia2RzbnlkZXJkZXYiLCJhIjoiY2swNnZ4ODB6MDEyZjNscGFuYXhnbzVrYyJ9.Yy8MxX3sxuGu0NVwxeFEJA';
 
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var mymap = L.map('mapid').setView([37.43, -107.43], 5);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapBoxAccessToken, {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -9,5 +9,8 @@ $(document).ready(function () {
         id: 'mapbox.streets',
         accessToken: mapBoxAccessToken
     }).addTo(mymap);
+
+    var sdMarker = L.marker([32.7157, - 117.1611]).addTo(mymap);
+    sdMarker.bindPopup("<b>9520 Town Centre Drive</b><br>https://advantagesolutions.net/").openPopup();
 
 });
